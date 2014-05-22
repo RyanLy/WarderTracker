38cb6ff36c4902e149523a8203ea4ed21c0f5e05
Ryan Ly <windask@hotmail.com>
2014-05-22 02:08:12 -0400

    removed debug statements

--- LeagueWardScraper.py
+++ LeagueWardScraper.py
@@ -8,19 +8,15 @@ def get_ward_count(name,apikey = "ebacf303-2d6a-4cda-b132-260e8155f0bc"):
 
 	summoner_name = name
 	url_summoner = "https://prod.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summoner_name + "?api_key=" + apikey
-	#print url_summoner
 	http = httplib2.Http()
 	_, content = http.request(url_summoner, 'GET')
 	data = json.loads(content)
-	#print data
+	#get summoner ID
 	summoner_id = data[summoner_name.lower()]['id']
-	#print summoner_id
-	#url_name = re.sub(" ","_",name)
 	url = "https://prod.api.pvp.net/api/lol/na/v1.3/game/by-summoner/" + str(summoner_id) + "/recent?api_key=ebacf303-2d6a-4cda-b132-260e8155f0bc"
 	http = httplib2.Http()
 	_, content = http.request(url, 'GET')
 	data = json.loads(content)
-	#print data
 
 	def traverse(data):
 		count = 0
@@ -37,6 +33,7 @@ def get_ward_count(name,apikey = "ebacf303-2d6a-4cda-b132-260e8155f0bc"):
 		return count
 	return traverse(data)
 
+#Goes through list of names and calculates # of wards
 counter = 0
 for name in names:
 	print name
@@ -45,3 +42,4 @@ for name in names:
 	if counter == 5:
 		#API Limit
 		time.sleep(10)
+
