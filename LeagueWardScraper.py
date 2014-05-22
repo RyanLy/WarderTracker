d27c97ea4eacebf2484a283e9da4aea179cec38b
Ryan Ly <windask@hotmail.com>
2014-05-22 02:56:22 -0400

    small fixes

--- LeagueWardScraper.py
+++ LeagueWardScraper.py
@@ -2,10 +2,10 @@ import httplib2
 import time 
 import json
 
-names = ['Windask','LightBrite','Khey','xBiscuits','Kottonbun','DragonSlayer965']
+names = ['Windask','LightBrite','Khey','xBiscuits','Kottonbun','DragonSlayer965','aePheva']
 
+#API supports up to 40 summoner GETs at a time. Maybe consider doing this to have less API requests
 def get_ward_count(name,apikey = "ebacf303-2d6a-4cda-b132-260e8155f0bc"):
-
 	summoner_name = name
 	url_summoner = "https://prod.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summoner_name + "?api_key=" + apikey
 	http = httplib2.Http()
@@ -13,17 +13,15 @@ def get_ward_count(name,apikey = "ebacf303-2d6a-4cda-b132-260e8155f0bc"):
 	data = json.loads(content)
 	#get summoner ID
 	summoner_id = data[summoner_name.lower()]['id']
-	url = "https://prod.api.pvp.net/api/lol/na/v1.3/game/by-summoner/" + str(summoner_id) + "/recent?api_key=ebacf303-2d6a-4cda-b132-260e8155f0bc"
+	url = "https://prod.api.pvp.net/api/lol/na/v1.3/game/by-summoner/" + str(summoner_id) + "/recent?api_key=" + apikey
 	http = httplib2.Http()
 	_, content = http.request(url, 'GET')
 	data = json.loads(content)
 
-	def traverse(data):
+	def get_ward_count_from_summoner(data):
 		count = 0
-		if not data:
-			return
-		data = data['games']
 		index = 0
+		data = data['games']
 		while index < len(data):
 			try:
 				count += data[index]['stats']['wardPlaced']
@@ -31,15 +29,19 @@ def get_ward_count(name,apikey = "ebacf303-2d6a-4cda-b132-260e8155f0bc"):
 				pass
 			index += 1
 		return count
-	return traverse(data)
+
+	return get_ward_count_from_summoner(data)
 
 #Goes through list of names and calculates # of wards
-counter = 0
-for name in names:
-	print name
-	print get_ward_count(name)
-	counter += 1
-	if counter == 5:
-		#API Limit
-		time.sleep(10)
+def get_ward_counts(names):
+	print "Ward Counts:"
+	counter = 0
+	for name in names:
+		print name + ": " + str(get_ward_count(name))
+		counter += 1
+		if counter == 4:
+			#API Limit
+			time.sleep(10)
+			counter = 0
 
+get_ward_counts(names)
